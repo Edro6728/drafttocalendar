2dd824d34e092b416416edbeb88ba51b5b1e04d0
Edro6728 <100054872+Edro6728@users.noreply.github.com>
2022-04-26 22:43:54 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "cdbea1fb2a335600740eb67a465b3084"
-api_id = 12875919
-bot_token = "5317395920:AAFC22fvsvHUbje8rH9EHgQYFYgEbXGziZs"
-bot_admin_group = -1001607137860
+api_hash = "2c0b01283b1b67f7087a92cc81cd91f8"
+api_id = 18403498
+bot_token = "5226841872:AAHF1LN3jQVZM-eiHqT-uxP6PQnI2O5meec"
+bot_admin_group = -1001755080513
